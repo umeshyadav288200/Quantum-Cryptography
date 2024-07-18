d38178df07c218ab970165043fcecbca288c89e0
umeshyadav2882003 <umeshyadav2882003@gmail.com>
2024-07-18 09:24:57 +0530

    quantum cryptography

---
+++ XOR_Bitwise_Encrypt_Decrypt_Using_Image_Key.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Umesh,Lokhnath
+
+XOR encrypt + decrypt using an image as Key
+
+"""
+
+
+
+import cv2
+ 
+
+demo = cv2.imread("OrionNebula.png",0)
+r, c = demo.shape
+
+
+key = cv2.imread("pseudorandomkey.png",0)
+
+
+ 
+encryption = cv2.bitwise_xor(demo, key) # encryption
+decryption = cv2.bitwise_xor(encryption, key) # decryption
+ 
+cv2.imshow("encryption", encryption) # Display ciphertext image
+cv2.imshow("decryption", decryption) # Display the decrypted image
+ 
+cv2.waitKey()
+cv2.destroyAllWindows()
